123c7276610f50876664c82b1e110b2fc69c12e2
tvogl <tvogl@ucsd.edu>
2021-09-09 09:24:32 -0700

    fall qtr restart

---
+++ examples/nlsy_diffs.R
@@ -0,0 +1,110 @@
+# uncomment if libraries are not installed.
+# install.packages("ggplot2")
+# install.packages("tidyverse")
+# install.packages("dplyr")
+# install.packages("estimatr")
+
+###########################################
+#ESTIMATING RACIAL DIFFERENCES IN EARNINGS#
+#IN THE NAT'L LONGITUDINAL SURVEY OF YOUTH#
+###########################################
+
+# input Stata file (need haven library for importing Stata file)
+library(haven)
+nlsy79 <- read_dta("https://github.com/tvogl/econ121/raw/main/data/nlsy79.dta")
+
+# data structure
+glimpse(nlsy79)
+
+# mean and sd of labor earnings (na.rm=TRUE removes missing values from the calculation)
+mean(nlsy79$laborinc07,na.rm=TRUE)
+sd(nlsy79$laborinc07,na.rm=TRUE)
+
+# more detailed summary with percentiles
+# note that there are many zeros, which implies
+# that we cannot take logarithms without first
+# thinking about sample selection.
+summary(nlsy79$laborinc07)
+
+# we can see this better when we plot histograms by race (need  ggplot2 library for graphing)
+library(ggplot2)
+ggplot(nlsy79, aes(x = laborinc07)) +
+  geom_histogram(fill = "white", colour = "black") +
+  facet_grid(black ~ .)
+# let's add value labels for race (need dplyr library for data manipulation)
+library(dplyr)
+nlsy79_labels <- nlsy79
+nlsy79_labels$black <- recode_factor(nlsy79_mod$black, '0' = 'Non-black', '1' = 'Black')
+ggplot(nlsy79_labels, aes(x = laborinc07)) +
+  geom_histogram(fill = "white", colour = "black") +
+  facet_grid(black ~ .)
+
+#we will estimate differences between blacks and non-blacks.
+
+# means by race: this uses the dplyr library and the pipe operator %>%
+nlsy79 %>% group_by(black) %>% summarize(mean=mean(laborinc07, na.rm = TRUE),
+                                         sd=sd(laborinc07, na.rm = TRUE),
+                                         n=sum(!is.na(laborinc07)))
+
+# these results give us all the information we need to test for differences by race.
+# difference
+44577-30214
+# t-statistic
+(44577-30214)/sqrt(53998^2/5150 + 34920^2/2278)
+# well above 1.96, so statistically significant by the usual standards.
+
+# alternative ways to run this test are...
+# t-test with unequal variances:
+t.test(laborinc07 ~ black, data = nlsy79)
+# regression with robust SEs (need to load estimatr library for lm_robust function)
+library(estimatr)
+lm_robust(laborinc07 ~ black,data = nlsy79)
+
+# it is actually uncommon to test for average differences in the level 
+# (rather than log) of earnings, including zeros from the non-employed. 
+# it would be much more typical to restrict to employed individuals. so 
+# let's restrict to people who worked at least 1000 hours in 2007:
+# equivalent to a part-time job of 20 hours per week for 50 weeks.
+summary(nlsy79$hours07)
+nlsy79_workers <- subset(nlsy79, hours07>=1000)
+summary(nlsy79_workers$hours07)
+# means by race
+nlsy79_workers %>% group_by(black) %>% summarize(mean=mean(laborinc07, na.rm = TRUE),
+                                                 sd=sd(laborinc07, na.rm = TRUE),
+                                                 n=sum(!is.na(laborinc07)))
+# still an $15k difference, which amounts to a black/non-black ratio of:
+55119/40731
+# note that the observation counts and means are slightly different from 
+# what we get in Stata. I am not sure why!
+
+# now let's look at log earnings. first remove zeros, then take logs.
+nlsy79_workers <- subset(nlsy79_workers, laborinc07>0)
+nlsy79_workers$loginc07 <- log(nlsy79_workers$laborinc07)
+nlsy79_workers %>% group_by(black) %>% summarize(mean=sprintf("%0.3f",mean(loginc07, na.rm = TRUE)),
+                                                 sd=sd(loginc07, na.rm = TRUE),
+                                                 n=sum(!is.na(loginc07)))
+# difference:
+10.639-10.344
+
+# this difference in logs can by roughly interpreted as a 30% difference 
+# in earnings, although this interpretation relies on calculus [dln(y)/dx],
+# so it is really for continuous X variables rather than binary ones. if we 
+# were being really careful, we would exponentiate the difference of logs:
+exp(10.639-10.344)
+# which implies a 34% difference in geometric means. this proportional 
+# difference is very close to the 36% difference in arithmetic means 
+# that we saw above.
+
+# the t-statistic:
+(10.639-10.344)/sqrt(.823^2/3829 + .925^2/1566)
+# well above 1.96, so statistically significant by the usual standards.
+
+# alternative ways to run this test are...
+# t-test with unequal variances:
+t.test(loginc07 ~ black, data = nlsy79_workers)
+# regression with heteroskedasticity-robust standard errors:
+lm_robust(loginc07 ~ black,data = nlsy79_workers)
+# same results. that is to say, a regression on a "dummy variable"
+# for black leads to the same results as a difference of means
+# note that the t-statistic is very slightly different from what
+# we computed "by hand." that's likely due to rounding errors.
--- examples/nlsy_diffs.do
+++ examples/nlsy_diffs.do
@@ -1,6 +1,5 @@
 cap log close
 log using nlsy_diffs.log,replace
-pause on
 
 *******************************************
 *ESTIMATING RACIAL DIFFERENCES IN EARNINGS*
@@ -11,22 +10,18 @@ use "https://github.com/tvogl/econ121/raw/main/data/nlsy79.dta",clear
 
 *list variables
 describe
-pause
 
 *show mean and sd of labor earnings
 sum laborinc07
-pause
 
 *more detailed summary with percentiles
 *note that there are many zeros, which implies
 *that we cannot take logarithms without first
 *thinking about sample selection.
 sum laborinc07,d
-pause
 
 *we can also see this by plotting histograms by race
 hist laborinc07,by(black) percent
-pause
 
 *we will be interested in estimating differences
 *between blacks and non-blacks.
@@ -34,17 +29,15 @@ pause
 *means by race. two methods:
 bysort black: sum laborinc07
 tab black,sum(laborinc07)
-pause
 
 *these results give us all the information we need 
 *to test for differences by race. we use the "display"
 *command, "di" for short, to perform calculations.
 *difference:
-di 47939.387-28484.452
+di 44577-30214
 *t-statistic:
-di (47939.387-28484.452)/sqrt(57507.716^2/3643 + 34586.784^2/562)
+di (44577-30214)/sqrt(53998^2/5150 + 34920^2/2278)
 *well above 1.96, so statistically significant by the usual standards.
-pause
 
 *alternative ways to run this test are...
 *t-test with unequal variances:
@@ -57,7 +50,6 @@ reg laborinc07 black,r
 *note also that the standard devitations are VERY
 *different. hence it is very important to allow for
 *unequal variances (or equivalently, heteroskedasticity).
-pause
 
 *it is actually uncommon to test for average differences
 *in the level (rather than log) of earnings, including
@@ -70,31 +62,31 @@ sum hours07,d
 keep if hours07>=1000
 *means of by race
 tab black,sum(laborinc07)
-*still an $18k difference, which amounts to a black/non-black
+*still an $15k difference, which amounts to a black/non-black
 *ratio of:
-di 58142/40512
-pause
+di 55186/40626
+
 *now let's look at log earnings.
 gen loginc07 = ln(laborinc07)
 tab black,sum(loginc07)
 *difference:
-di 10.681949-10.332826
-pause
+di 10.635-10.340
+
 *this difference in logs can by roughly interpreted as 
-*a 35% difference in earnings, although this interpretation
+*a 30% difference in earnings, although this interpretation
 *relies on calculus [dln(y)/dx], so it is really for
 *continuous X variables rather than binary ones. if we 
 *were being really careful, we would exponentiate the 
 *difference of logs:
-di exp(10.681949-10.332826)
-*which implies a 42% difference in geometric means. this
-*proportional difference is very close to the 44% difference
+di exp(10.635-10.340)
+*which implies a 34% difference in geometric means. this
+*proportional difference is very close to the 36% difference
 *in arithmetic means that we saw above.
-pause
+
 *the t-statistic:
-di (10.681949-10.332826)/sqrt(.84377072^2/2792 + .9771058^2/364)
+di (10.635-10.340)/sqrt(.832^2/3865 + .927^2/1572)
 *well above 1.96, so statistically significant by the usual standards.
-pause
+
 *alternative ways to run this test are...
 *t-test with unequal variances:
 ttest loginc07,by(black) unequal
@@ -102,7 +94,6 @@ ttest loginc07,by(black) unequal
 reg loginc07 black,r
 *same results. that is to say, a regression on a "dummy variable"
 *for black leads to the same results as a difference of means
-
 
 log close
 
