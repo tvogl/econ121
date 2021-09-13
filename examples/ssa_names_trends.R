ec73988116c0791aecc6ff73f9f6f0a8fde12009
tvogl <tvogl@ucsd.edu>
2021-09-13 10:59:08 -0700

    ssa names update

---
+++ examples/ssa_names_trends.R
@@ -0,0 +1,47 @@
+# uncomment if libraries are not installed.
+# install.packages("tidyverse")
+
+# analyzes naming trends in the social security names database (national).
+# takes special interest in frequency of muslim names before and after 9/11/2001.
+
+# input Stata file (need haven library for importing Stata file)
+library(haven)
+ssa <- read_dta("https://github.com/tvogl/econ121/raw/main/data/ssa_names.dta")
+
+# summarize
+summary(ssa)
+
+# data structure (need tidyverse library for the glimpse function)
+library(tidyverse)
+glimpse(ssa)
+
+# create post 1990 data frame, since we are interested in 9/11
+# also keep only boy names, and only Osama, Mohammed, and Ahmed
+data <- ssa[which(ssa$year>=1990 & ssa$sex=="M" & ssa$name %in% c("Osama", "Mohammed", "Ahmed")), ]
+
+# summarize and glimpse new data frame
+summary(data)
+glimpse(data)
+
+# graph counts of the three names, with a vertical line at 2001
+# we will use the ggplot2 library, which is already loaded in the tideverse
+ggplot(data, aes(x=year, y=freq, group=name, color=name)) +
+  geom_line() +
+  geom_vline(xintercept=2001)
+
+# large levels differences --> counts awkward to compare
+# let's take logs, so we can study proportional changes	  
+data$lnfreq = log(data$freq)
+ggplot(data, aes(x=year, y=lnfreq, group=name, color=name)) +
+  geom_line() +
+  geom_vline(xintercept=2001)
+
+
++
+  scale_color_viridis(discrete = TRUE) +
+  theme(legend.position="none") +
+  ggtitle("Popularity of American names in the previous 30 years") +
+  theme_ipsum()
+  
+
+
---
+++ examples/ssa_names_trends.do
@@ -0,0 +1,39 @@
+//analyzes naming trends in the social security names database (national).
+//takes special interest in frequency of muslim names before and after 9/11/2001.
+
+//open dataset
+use "https://github.com/tvogl/econ121/raw/main/data/ssa_names.dta",clear
+
+//summarize
+sum
+
+//structure of data - look at first 10 observations
+list if _n<=10
+
+//keep post 1990, since we are interested in 9/11
+tab year
+keep if year>=1990
+
+//keep boy names only, since we are especially interested in Osama
+tab sex
+keep if sex=="M"
+
+//note the difference in syntax: year is numeric, sex is string
+
+//graph counts of three male muslim names, with a vertical line at 2001
+twoway (line freq year if name=="Osama",lcolor(black) lwidth(thick)) ///
+       (line freq year if name=="Mohammed",lcolor(orange)) ///
+       (line freq year if name=="Ahmed",lcolor(blue)) ///
+       ,xline(2001) ///
+	    legend(label(1 "Osama") label(2 "Mohammed") ///
+		       label(3 "Ahmed") rows(1))
+
+//large levels differences --> counts awkward to compare
+//let's take logs, so we can study proportional changes	   
+gen lnfreq = ln(freq)		
+twoway (line lnfreq year if name=="Osama",lcolor(black) lwidth(thick)) ///
+       (line lnfreq year if name=="Mohammed",lcolor(orange)) ///
+       (line lnfreq year if name=="Ahmed",lcolor(blue)) ///
+       ,xline(2001) ///
+	    legend(label(1 "Osama") label(2 "Mohammed") ///
+		       label(3 "Ahmed") rows(1))
